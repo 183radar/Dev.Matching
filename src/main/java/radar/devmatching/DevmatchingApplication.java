dd13a15dbe20204b3412070ec8532cf55b64ff31
choihuk <love72huk@naver.com>
2022-11-19 16:13:52 +0900

    project setting

---
+++ src/main/java/radar/devmatching/DevmatchingApplication.java
@@ -0,0 +1,13 @@
+package radar.devmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/radar/devmatching/DevmatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package radar.devmatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
