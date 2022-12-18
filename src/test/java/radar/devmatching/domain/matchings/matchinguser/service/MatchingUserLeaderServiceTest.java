ad9baf2f9b0c5a4ab9ca1008fba852b14ccd0ba4
tlarbals824 <simkm0824@naver.com>
2022-12-18 15:59:36 +0900

    User builder id 설정 리플렉션으로 변경

--- src/test/java/radar/devmatching/domain/matchings/matchinguser/service/MatchingUserLeaderServiceTest.java
+++ src/test/java/radar/devmatching/domain/matchings/matchinguser/service/MatchingUserLeaderServiceTest.java
@@ -2,6 +2,8 @@ package radar.devmatching.domain.matchings.matchinguser.service;
 
 import static org.mockito.BDDMockito.*;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,9 +32,8 @@ class MatchingUserLeaderServiceTest {
 		matchingUserLeaderService = new MatchingUserLeaderServiceImpl(matchingUserRepository);
 	}
 
-	private User createUser() {
+	private User basicUser() {
 		return User.builder()
-			.id(TEST_USER_ID)
 			.username("username")
 			.password("password")
 			.nickName("nickName")
@@ -42,9 +43,20 @@ class MatchingUserLeaderServiceTest {
 			.build();
 	}
 
+	private User createUser() throws NoSuchFieldException, IllegalAccessException {
+		User user = basicUser();
+
+		Class<User> userClass = User.class;
+		Field userId = userClass.getDeclaredField("id");
+		userId.setAccessible(true);
+		userId.set(user, TEST_USER_ID);
+
+		return user;
+	}
+
 	@Test
 	@DisplayName("createMatchingUserLeader 메서드는 user 엔티티와 matching 엔티티 입력 받으면 leader 상태를 담은 matchingUser 엔티티를 생성한다.")
-	public void createMatchingUserLeaderMethod() {
+	public void createMatchingUserLeaderMethod() throws NoSuchFieldException, IllegalAccessException {
 		//given
 		User user = createUser();
 		Matching matching = Matching.builder().build();
