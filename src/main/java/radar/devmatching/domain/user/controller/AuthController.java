6c8b848aee9b01e475665caf965debc8a3ad2aef
tlarbals824 <simkm0824@naver.com>
2022-12-26 15:13:30 +0900

    오류수정

--- src/main/java/radar/devmatching/domain/user/controller/AuthController.java
+++ src/main/java/radar/devmatching/domain/user/controller/AuthController.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,16 +27,22 @@ import radar.devmatching.domain.user.service.dto.response.SignOutResponse;
  *  TODO : RestController -> Controller 변경
  */
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class AuthController {
 
 	private final AuthService authService;
 
+	@GetMapping("/signIn")
+	public String signInPage(Model model) {
+		model.addAttribute("signInRequest", SignInRequest.of());
+		return "/user/signIn";
+	}
+
 	// TODO : SecurityContextHolder 에 Authentication 저장하기 추가.
 	@PostMapping("/signIn")
-	public void signIn(HttpServletResponse response, @ModelAttribute SignInRequest signInRequest,
+	public String signIn(HttpServletResponse response, @ModelAttribute SignInRequest signInRequest,
 		BindingResult bindingResult) {
 		SignInResponse signInResponse = authService.signIn(signInRequest.getUsername(), signInRequest.getPassword());
 
@@ -52,18 +59,18 @@ public class AuthController {
 		JwtCookieProvider.setCookie(response, accessTokenCookie);
 		JwtCookieProvider.setCookie(response, refreshTokenCookie);
 		log.info("signIn execute");
-		// return "/";
+		return "redirect:/";
 	}
 
 	@GetMapping("/signOut")
-	public void signOut(HttpServletRequest request, HttpServletResponse response, @AuthUser User user) {
+	public String signOut(HttpServletRequest request, HttpServletResponse response, @AuthUser User user) {
 		SignOutResponse signOutResponse = authService.singOut();
 		log.info("access User={}", user);
 		log.info("logout process execute");
 		JwtCookieProvider.deleteCookieFromRequest(request, response, signOutResponse.getAccessTokenHeader());
 		JwtCookieProvider.deleteCookieFromRequest(request, response, signOutResponse.getRefreshTokenHeader());
 
-		// return "/";
+		return "redirect:/api/users/signIn";
 	}
 
 }
