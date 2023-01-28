3d2971e9bd69f5fc49b36855ca84dcd05b255dc8
choihuk <love72huk@naver.com>
2023-01-28 17:32:10 +0900

    fix Controller jwt

--- src/test/java/radar/devmatching/util/WithCustomUserSecurityContextFactory.java
+++ src/test/java/radar/devmatching/util/WithCustomUserSecurityContextFactory.java
@@ -2,34 +2,30 @@ package radar.devmatching.util;
 
 import java.util.ArrayList;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
-import org.springframework.test.util.ReflectionTestUtils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.impl.DefaultClaims;
+import radar.devmatching.common.security.JwtProperties;
 import radar.devmatching.common.security.jwt.JwtAuthenticationToken;
-import radar.devmatching.domain.user.entity.User;
 
 public class WithCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithCustomUser> {
 
 	@Override
 	public SecurityContext createSecurityContext(WithCustomUser customUser) {
-
-		User user = User.builder()
-			.username(customUser.username())
-			.build();
-		ReflectionTestUtils.setField(user, "id", 1L);
-		user.changeUserRole(customUser.userRole());
-		// UserDetails principal = new CustomUserDetails(user);
-
 		ArrayList<SimpleGrantedAuthority> authorities = new ArrayList<>();
 		authorities.add(new SimpleGrantedAuthority(customUser.userRole().toString()));
 
-		Authentication auth = new JwtAuthenticationToken(customUser.username(), "", authorities);
+		Claims claims = new DefaultClaims();
+		claims.put(JwtProperties.USER_ID, "1");
+		claims.put(JwtProperties.USERNAME, "CustomUsername");
+
+		JwtAuthenticationToken authentication = new JwtAuthenticationToken(claims, "", authorities);
 		SecurityContext context = SecurityContextHolder.createEmptyContext();
-		context.setAuthentication(auth);
+		context.setAuthentication(authentication);
 		return context;
 	}
 
